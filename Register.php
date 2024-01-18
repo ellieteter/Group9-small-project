554ef281cfb3d3fc4e0313574ea11b1737084102
Ethan Fuller <etjamir14@gmail.com>
2024-01-18 15:09:52 -0500

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,60 +1,67 @@
 <?php
 
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
 
-    $conn = new mysqli("localhost", "API", "Ethan", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
+$conn = new mysqli("localhost", "API", "Ethan", "COP4331");
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
 
-    else
-	{
+    //Check if they are registered already
+    $checkstmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
 
-        //TODO 
-        //Check if they are registered already
-        
-		$stmt = $conn->prepare("INSERT INTO Users (firstName,lastName,Login,Password) VALUES(?,?,?,?)");
+    $checkstmt->bind_param("s", $inData["login"]);
+    $checkstmt->execute();
+
+    $checkresult = $checkstmt->get_result();
+
+    if (checkresult->num_rows > 0) {
+        //User is already registered
+        returnWithError("User already registered with this login");
+    } else {
+        $stmt = $conn->prepare("INSERT INTO Users (firstName,lastName,Login,Password) VALUES(?,?,?,?)");
         $stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($stmt->affected_rows > 0) {
             returnWithInfo($inData["firstname"], $inData["lastname"], $conn->insert_id);
-        }
-        else {
+        } else {
 
             returnWithError("Unable to Create the Record");
         }
 
-		$stmt->close();
-		$conn->close();
-	}
+        $stmt->close();
+        $conn->close();
+    }
+
+    $checkstmt->close();
+}
 
 
-    function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($firstName, $lastName, $id)
+{
+    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
 
 
 
