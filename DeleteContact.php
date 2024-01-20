8f633dd5556c78d99bc015b41ea0129ed7342fba
Ethan Fuller <etjamir14@gmail.com>
2024-01-20 12:24:53 -0500

    Update DeleteContact.php

---
+++ DeleteContact.php
@@ -0,0 +1,39 @@
+<?php
+
+$inData = getRequestInfo();
+
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
+$userID = $inData["userID"];
+
+$conn = new mysqli("localhost", "API", "Ethan", "COP4331");
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE (firstName = ? AND lastName = ? AND phone = ? AND email = ? AND userID = ?)");
+    $stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $userID);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
+}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
